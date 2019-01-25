e0d85fd5793367e8d99fd31f0ad622a2f37894be
Andie <zchangan@163.com>
2019-01-25 13:22:55 +0800

    :construction: 首页、动态iframe、动态首页、空间 完成

--- bilibili-watchlater-plus.user.js
+++ bilibili-watchlater-plus.user.js
@@ -7,123 +7,146 @@
 // @include      /(.+.)?bilibili.com/
 // @description  Bilibili 稍后再看功能增强
 // @version      0.0.1
+// @grant        GM_addStyle
 // @require      https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js
 // ==/UserScript==
 
 (async function () {
   'use strict';
-  // -----------------hint-----------------
+  // -----------------Constant-----------------
+  const BUTTON_CLASS_NAME = 'watch-later-plus';
+  const CONTAINER_CLASS_NAME = 'watch-later-plus-container';
+  const HINT_CLASS_NAME = 'watch-later-plus-hint';
+
+  const DEBOUNCE_WAIT = 200;
+  // const SKIP_TOKEN = 'data-watch-later-plus-skip';
+  // -----------------Constant-----------------
+
+  // -----------------Hint-----------------
   const _hint = document.createElement('div');
-  _hint.classList.add('watch-later-hint');
-  _hint.style.position = 'absolute';
-  _hint.style.fontSize = '12px';
-  _hint.style.color = '#fff';
-  _hint.style.borderRadius = '4px';
-  _hint.style.lineHeight = '18px';
-  _hint.style.padding = '4px 8px';
-  _hint.style.zIndex = '99999';
-  _hint.style.backgroundColor = '#000';
+  _hint.className = HINT_CLASS_NAME;
   document.body.appendChild(_hint);
   function hideHint () {
     _hint.style.display = 'none';
   }
   function showHint (str, target) {
     _hint.innerText = str;
-    _hint.style.display = 'block';
     target = $(target);
     _hint.style.top = (target.offset().top - 30) + 'px';
     _hint.style.left = (target.offset().left - $(_hint).width() / 2) + 'px';
+    _hint.style.display = 'block';
   }
-  // -----------------hint-----------------
+  // -----------------Hint-----------------
 
   // -----------------Utils-----------------
   // 替换首页、动态iframe
-  function replaceWatchLaterTrigger (root) {
-    const list = $(root).find('.watch-later-trigger');
+  const replaceIndex = _.debounce(function () {
+    const list = $(`.watch-later-trigger`);
+    if (list.length) {
+      console.log('replaceIndex', list.length);
+    }
     list.each((_, ele) => {
-      if (ele.dataset && ele.dataset.skipMutation) {
-        console.log('replaceWatchLaterTrigger skip');
-      } else {
-        const href =
+      const href =
         $(ele).parents('a[data-target-url]').attr('data-target-url') ||
         $(ele).parents('a[href]').attr('href') ||
         $(ele).siblings('a[href]').attr('href');
-        const aid = /av(\d+)/.exec(href)[1];
-        const clone = createWatchLater(aid, 'watch-later-trigger w-later watch-later', 'added');
-        ele.replaceWith(clone);
-      }
+      const aid = /av(\d+)/.exec(href)[1];
+      const clone = createWatchLater(aid, ele.parentElement);
+      ele.replaceWith(clone);
     });
-  }
-  // 替换空间
-  function replaceIWatchLater (root) {
-    const list = $(root).find('.i-watchlater');
-    list.each((_, ele) => {
-      if (ele.dataset && ele.dataset.skipMutation) {
-        console.log('replaceIWatchLater skip');
-      } else {
-        const href =
-        $(ele).parents('a[href]').attr('href');
-        if (href === 'javascript:;') {
-          console.log('该视频已失效，跳过');
-        } else {
-          const aid = /av(\d+)/.exec(href)[1];
-          const clone = createWatchLater(aid, 'i-watchlater', 'has-select');
-          ele.replaceWith(clone);
-        }
-      }
-    });
-  }
-  // 替换动态首页
-  function replaceSeeLater (root) {
-    const list = $(root).find('.see-later');
-    list.each((_, ele) => {
-      if (ele.dataset && ele.dataset.skipMutation) {
-        console.log('replaceSeeLater skip');
-      } else {
-        const href =
-        $(ele).parents('a[href]').attr('href');
-        const aid = /av(\d+)/.exec(href)[1];
-        const clone = createWatchLater(aid, 'see-later', 'done');
-        Object.assign(clone.dataset, ele.dataset);
-        ele.replaceWith(clone);
-      }
-    });
-  }
-  // 处理首页番剧空白的问题
-  function handleEmptyIndexBangumi () {
-    const list = $('.spread-module:not(:has(.watch-later-trigger))');
+  }, DEBOUNCE_WAIT);
+
+  // 处理首页番剧
+  const handleIndexBangumi = _.debounce(function () {
+    const list = $(`#bili_bangumi .spread-module:not(:has(.${BUTTON_CLASS_NAME}))`);
+    if (list.length) {
+      console.log('handleIndexBangumi', list.length);
+    }
     list.each((_, ele) => {
       const href = $(ele).find('a[href]').attr('href');
       const aid = /av(\d+)/.exec(href)[1];
-      const clone = createWatchLater(aid, 'watch-later-trigger w-later watch-later', 'added');
+      const clone = createWatchLater(aid, ele);
       $(ele).find('.pic').append(clone);
     });
-  }
-  // 处理动态iframe番剧空白的问题
-  function handleEmptyDynamicBangumi () {
-    const list = $('.d-data>.preview>a[href]:not(:has(.watch-later-trigger))');
+  }, DEBOUNCE_WAIT);
+
+  // 处理动态iframe番剧
+  const handleDynamicIframeBangumi = _.debounce(function () {
+    const list = $(`.d-data>.preview>a:not(:has(.${BUTTON_CLASS_NAME}))`);
+    if (list.length) {
+      console.log('handleDynamicIframeBangumi', list.length);
+    }
     list.each(async (_, ele) => {
       const href = ele.href;
-      const aid = await bangumiAid(href);
-      const clone = createWatchLater(aid, 'watch-later-trigger w-later watch-later', 'added');
+      const epid = /ep(\d+)/.exec(href)[1];
+      const aid = await epid2aid(epid);
+      const clone = createWatchLater(aid, ele);
       ele.appendChild(clone);
     });
-  }
-  function createWatchLater (aid, className, done) {
+  }, DEBOUNCE_WAIT);
+
+  // 替换空间
+  const replaceSpace = _.debounce(function (root) {
+    const list = $('span.i-watchlater');
+    if (list.length) {
+      console.log('replaceSpace', list.length);
+    }
+    list.each((_, ele) => {
+      const href = $(ele).parents('a[href]').attr('href');
+      if (href === 'javascript:;') {
+        console.log('该视频已失效，跳过');
+      } else {
+        const aid = /av(\d+)/.exec(href)[1];
+        const clone = createWatchLater(aid, ele.parentElement);
+        ele.replaceWith(clone);
+      }
+    });
+  }, DEBOUNCE_WAIT);
+
+  // 替换动态首页
+  const replaceDynamicIndex = _.debounce(function (root) {
+    const list = $('.see-later');
+    if (list.length) {
+      console.log('replaceDynamicIndex', list.length);
+    }
+    list.each((_, ele) => {
+      const href = $(ele).parents('a[href]').attr('href');
+      const aid = /av(\d+)/.exec(href)[1];
+      const clone = createWatchLater(aid, ele.parentElement);
+      ele.replaceWith(clone);
+    });
+  }, DEBOUNCE_WAIT);
+
+  // 处理动态首页番剧
+  const handleDynamicIndexBangumi = _.debounce(function handleDynamicIndexBangumi () {
+    const list = $(`.bangumi-container .image-area:not(:has(.${BUTTON_CLASS_NAME}))`);
+    if (list.length) {
+      console.log('handleDynamicIndexBangumi', list.length);
+    }
+    list.each(async (_, ele) => {
+      const href = $(ele).parents('a[href]').attr('href');
+      const epid = /ep(\d+)/.exec(href)[1];
+      const aid = await epid2aid(epid);
+      const clone = createWatchLater(aid, ele);
+      ele.appendChild(clone);
+    });
+  }, DEBOUNCE_WAIT);
+
+  function createWatchLater (aid, parent) {
+    parent.classList.add(CONTAINER_CLASS_NAME);
     const clone = document.createElement('div');
-    clone.dataset.skipMutation = 'true';
-    clone.className = className;
+    clone.className = BUTTON_CLASS_NAME;
     clone.dataset.aid = aid;
     watchLaterList().then(list => {
       if (list.includes(aid)) {
-        clone.classList.add(done);
-        clone.dataset.added = true;
+        clone.dataset.checked = true;
       }
     });
     clone.onmouseenter = (event) => {
       const target = event.currentTarget;
-      if (target.dataset.added) {
+      if (target.dataset.checked) {
         showHint('移除稍后再看+', target);
       } else {
         showHint('稍后再看+', target);
@@ -136,15 +159,13 @@
       event.stopPropagation();
       event.preventDefault();
       const target = event.currentTarget;
-      if (target.dataset.added) {
+      if (target.dataset.checked) {
         await removeWatchLater(target.dataset.aid);
-        target.removeAttribute('data-added');
-        clone.classList.remove(done);
+        target.removeAttribute('data-checked');
         showHint('稍后再看+', target);
       } else {
         await addWatchLater(target.dataset.aid);
-        clone.dataset.added = true;
-        clone.classList.add(done);
+        clone.dataset.checked = true;
         showHint('移除稍后再看+', target);
       }
     };
@@ -188,28 +209,60 @@
     });
     if (data.code !== 0) throw new Error(data.message);
   }
-  async function bangumiAid (link) {
-    const { data } = await axios.get(link);
+  async function epid2aid (epid) {
+    const { data } = await axios.get(`https://www.bilibili.com/bangumi/play/ep${epid}`);
     const initState = JSON.parse(/window.__INITIAL_STATE__=(.*?);/.exec(data)[1]);
     const aid = initState.epInfo.aid;
-    return aid;
+    return aid.toString();
+  }
+  function addStyle () {
+    GM_addStyle(`
+      .${BUTTON_CLASS_NAME} {
+        width: 22px;
+        height: 22px;
+        position: absolute;
+        right: 8px;
+        bottom: 4px;
+        opacity: 0;
+        cursor: pointer;
+        z-index: 99999;
+        background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABT0lEQVQ4je3VMYrCQBTG8b/DWASLNKJgJ1hY2sloZeF2gkeYW+wZvIVXsLBJY5UdtLJLk17EIghCwChukWRxFzSaaLdfMwm8/EgemTclgOl02gEmwAAoky8RsAA+tdbrUoJ+AVZO8G9CoCeJ39Sq1+sopbBtO5e23+8xxrDdbi1gIog/vxAKYNs2Sqn0diBIenoP9X2fIAgyccv66WZZZBXvdjtc18UYkwlfJxM+n8+/1pfBefMPPw4LEZcEQcBqteJ0Or0GrlartFotLpcLnucxm83YbDbFYSEE/X6f4XBIpVLhcDjgOA6u63I8HvPDaRqNBuPxmHa7DcS7cT6f36yXj8IAUkq63S7NZpPlcomUtx9/Ck5Tq9UYjUZ3a976u0UQz9OiCcMwvYwk8XHyYYxBKXU9+p5Grybg4m1Hk9Bar4Ee4JC0JWeixOhprdffE/1yRW/TLMYAAAAASUVORK5CYII=);
+      }
+      
+      .${BUTTON_CLASS_NAME}[data-checked] {
+        background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABiklEQVQ4jbXVzyvDcRzH8efns1+JIRtbOSiKrMRBrZZC5IzcHJYTJyc1J464ubuY5MQ/oLSTNSHfIg5KyEmbzY/5sa++c/hOCtv47rvX8f2pR+/3u0+fjwAgfNoFLAH9gA1jUYEIECLoU0QOjQIVBsHveQECEr1Ts1By1pJEH9/s9EuM77RQbLIMKACG4QaHhYNBL7EBj3lwtU2yFXDT6rRil8Ic2C4F634XnbU24m8aY9F46bAUsNJdR1+9g6f3LKPROBfp99LhxY5aRhorULUs43sJlFQmfxO/FWMDHpQhL+3VXzdxps3JVEsVAJOHSSK3rwWbsP5WtAhorrSy09vAxH6CeoeFOV8NALPHKTZvnovMBoLwafZ7sc4uWfe76HE70HKnUsDy+SNzJ/dFUciziruMxvBunNXLNFLo6Mb1M/N/RCHPKgAyWpbpoyQHdxmaKi0snD3wYzQj8GfWrtL/4L5S1rdCLYOrSvTvxOxEJBBC/07MygsQkgR9ChAAtiltLWrOCBD0KR9smmovo1v+1QAAAABJRU5ErkJggg==);
+      }
+
+      .${CONTAINER_CLASS_NAME}:hover .${BUTTON_CLASS_NAME} {
+        opacity: 1;
+      }
+
+      .${HINT_CLASS_NAME} {
+        position: absolute;
+        font-size: 12px;
+        color: #fff;
+        border-radius: 4px;
+        line-height: 18px;
+        padding: 4px 8px;
+        z-index: 99999;
+        background-color: #000;
+      }
+    `);
   }
   // -----------------Utils-----------------
 
   // -----------------Run Immediately-----------------
+  addStyle();
   hideHint();
   new MutationObserver((mutationList) => {
     for (const mutation of mutationList) {
-      for (const one of mutation.addedNodes) {
-        if (one.dataset && one.dataset.skipMutation) {
-          console.log('MutationObserver skip');
-          continue;
-        }
-        replaceWatchLaterTrigger(one);
-        replaceIWatchLater(one);
-        replaceSeeLater(one);
-        handleEmptyIndexBangumi();
-        handleEmptyDynamicBangumi();
+      if (mutation.addedNodes.length) {
+        replaceIndex();
+        handleIndexBangumi();
+        handleDynamicIframeBangumi();
+        replaceSpace();
+        replaceDynamicIndex();
+        handleDynamicIndexBangumi();
       }
     }
   }).observe(document.body, {
@@ -218,14 +271,15 @@
   });
   // t.bilibili.com/pages/nav/index
   // www.bilibili.com
-  replaceWatchLaterTrigger(document.body);
-  // space.bilibili.com
-  replaceIWatchLater(document.body);
-  // t.bilibili.com
-  replaceSeeLater(document.body);
+  replaceIndex();
   // www.bilibili.com
-  handleEmptyIndexBangumi();
+  handleIndexBangumi();
   // t.bilibili.com/pages/nav/index
-  handleEmptyDynamicBangumi();
+  handleDynamicIframeBangumi();
+  // space.bilibili.com
+  replaceSpace();
+  // t.bilibili.com
+  replaceDynamicIndex();
+  handleDynamicIndexBangumi();
   // -----------------Run Immediately-----------------
 })();
